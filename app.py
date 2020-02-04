6a19294d282412aa70319747fe463a3e82271032
shinilt <shinilstar@gmail.com>
2020-02-04 23:20:22 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -18,20 +18,25 @@ def generatetable():
     xmldata = '{"Message": "Hi there, we didnt get your message","ResponseStatus": "Waiting"}'
     xmldata = request.data #get the xml content from the request
     #print(xmldata)
+    xmldictionary=xmltodict.parse(xmldata)
     jsonstring=(json.dumps(xmltodict.parse(xmldata))) # this create an jsonstring-we can directly use jsonloads operation
     jsonobject=json.loads(jsonstring)
+
     # we need to filter out the required json elements here
-    for activity in jsonobject[["pd:ProcessDefinition"["pd:activity"["pd:resourceType"=="ae.activities.MapperActivity"]]]]:
-        jsonactivity=activity
+    for pd in jsonobject:
+        for activity in pd['pd:activity']:
+            jsonactivity=activity
+
+    """
 
     for inneractivity in jsonobject[["pd:ProcessDefinition"["pd:group"["pd:activity"["pd:resourceType" == "ae.activities.MapperActivity"]]]]]:
         jsonactivityinner=inneractivity
 
-
+"""
 
     build_direction = "LEFT_TO_RIGHT"
     table_attributes = {"style": "width:100%", "border": 1, "border-collapse": "collapse"}
-    tableout = json2table.convert(jsonactivityinner, build_direction=build_direction, table_attributes=table_attributes)
+    tableout = json2table.convert(jsonactivity, build_direction=build_direction, table_attributes=table_attributes)
     return tableout
 
 if __name__ == "__main__":
