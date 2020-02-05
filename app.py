89444e775712717ac7694fabdfcb408ec306b95b
shinilt <shinilstar@gmail.com>
2020-02-05 20:57:56 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -10,33 +10,69 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return "The service is running."
+    return """
+    <i>Service Health - Good</i>
+    <br>
+    <h1><u> TIBCO Mapping Exporter</u> </h1>
+    This application provides below functionality.
+     <br>
+    The API can be used to export the mapping defined in the TIBCO BW 5.x processes.
+    <br>
+    The scope is to extract the mapping content of the Map Data activity only. The response can be copied to excel in tabular form.
+    <br>
+    Basic exposure to xml is required to understand and interpret the mapping exported to the table.
+    <br><br><br>
+    <u>API Details</u> <br><br>
+    API URI- /generatetable
+    <br>
+    API Method - POST,GET (both work the same for simplicity).
+    <br>
+    API Input-  TIBCO BW 5.x process definition xml content in full(.process file content).
+    <br>
+    API Response -  Tabular format  of the mappings defined all the Map Data activities.
+    
+    <br><br><br><br><br>
+    Disclaimer- The data you submit is neither logged nor archived. If you dump trash, you will be returned trash. Developed as a personal project.
+    """
 
 
 @app.route("/generatetable", methods=['GET', 'POST'])
 def generatetable():
-    # make sure data is sent to data
+    #define the functions
+    def createTable(xmlinput):
+        jsonstring=(json.dumps(xmltodict.parse(xmlinput))) # this create an jsonstring-we can directly use jsonloads operation
+        jsonobject=json.loads(jsonstring)
+            #set table props
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style": "width:100%", "border": 1, "border-collapse": "collapse"}
+        tableout = json2table.convert(jsonobject, build_direction=build_direction, table_attributes=table_attributes)
+        return tableout
+    # processing start for uri
+    try:
+        xmlstringdata = request.data #get the xml string content from the request
+        # parse the xml
+        root = ET.fromstring(xmlstringdata)
+        #create dictionary of needed namespaces for use later to parse
+        myxmlnamespaces={'pd':'http://xmlns.tibco.com/bw/process/2003'}
+        allTableOutput = ""
+        #use xpath to filter the mapper activity in groups
 
-    xmldata = request.data #get the xml content from the request
+        for activity in root.findall("./pd:group/pd:activity[pd:resourceType='ae.activities.MapperActivity']",myxmlnamespaces):
 
-    root = ET.fromstring(xmldata)
+            activityxml=(ET.tostring(activity, encoding='utf8').decode('utf8'))
+            allTableOutput=allTableOutput+'<br>'+createTable(activityxml)
+        # use xpath to filter the root mapper activities
+        for activity in root.findall("./pd:activity[pd:resourceType='ae.activities.MapperActivity']",myxmlnamespaces):
 
-    for movie in root.findall("./pd:processDefinition/pd:activity[pd:type='com.tibco.plugin.xml.XMLParseActivity']"):
-        print(movie)
-        smallxml=(ET.tostring(movie, encoding='utf8').decode('utf8'))
+            activityxml=(ET.tostring(activity, encoding='utf8').decode('utf8'))
+            allTableOutput=allTableOutput+'<br>'+createTable(activityxml)
+
+        #retun response for the api
+        return allTableOutput
+    except:
+        return('Something is not right.  Please check the input xml,fix it and get back.')
 
 
-
-    #print(xmldata)
-    xmldictionary=xmltodict.parse(smallxml)
-    jsonstring=(json.dumps(xmltodict.parse(xmldata))) # this create an jsonstring-we can directly use jsonloads operation
-    jsonobject=json.loads(jsonstring)
-
-
-    build_direction = "LEFT_TO_RIGHT"
-    table_attributes = {"style": "width:100%", "border": 1, "border-collapse": "collapse"}
-    tableout = json2table.convert(jsonobject, build_direction=build_direction, table_attributes=table_attributes)
-    return tableout
 
 if __name__ == "__main__":
     app.run(debug=True)
